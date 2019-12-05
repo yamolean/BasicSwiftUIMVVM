4ff346bdb4f711aa3e2c65f04e0830f18e4485d7
yamolean <moririnmanro9608@gmail.com>
2019-12-06 02:55:36 +0900

    firstCommit

---
+++ BasicMVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BasicMVVM
+//
+//  Created by 矢守叡 on 2019/12/06.
+//  Copyright © 2019 yamolean. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
