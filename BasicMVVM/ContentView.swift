33c301bedd0196d06afd88080707e286995e6227
yamolean <moririnmanro9608@gmail.com>
2019-12-06 04:01:45 +0900

    add DispatchQueue at viewModel method

--- BasicMVVM/ContentView.swift
+++ BasicMVVM/ContentView.swift
@@ -33,36 +33,46 @@ class PhotoViewModel: ObservableObject {
     func URLsseion() {
         guard let url = URL(string: url) else { return }
         URLSession.shared.dataTask(with: url) { (data, response, error) in
+            DispatchQueue.main.async {
                 do {
                     self.photos = try JSONDecoder().decode([Photo].self, from: data!)
                 } catch {
                     print("Failed to decode JSON:", error)
                 }
+            }
         }.resume()
     }
 }
 
 struct ContentView: View {
+    @ObservedObject var photoViewModel = PhotoViewModel()
+    
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack(alignment: .leading) {
-                    //ここでforでtextにtitle,thumbnailUrlを入れまくる
-                    HStack {
-                        Text("title")
-                            .font(.system(size: 20))
-                            .fontWeight(.semibold)
-                            .background(Color.red)
-                        Spacer()
-                        
-                    }.padding(.bottom, 4)
-                    Text("thumbnailUrl")
-                        .padding(.bottom, 16)
-                        .background(Color.blue)
+                    ForEach(photoViewModel.photos) { photo in
+                        HStack {
+                            Text(photo.title)
+                                .font(.system(size: 20))
+                                .fontWeight(.semibold)
+                                .background(Color.red)
+                            Spacer()
+                            
+                        }.padding(.bottom, 4)
+                        Text(photo.thumbnailUrl)
+                            .padding(.bottom, 16)
+                            .background(Color.blue)
+                    }
                 }.padding(.horizontal, 22)
                     .padding(.top, 8)
             }
-            .navigationBarTitle("タイムライン",displayMode: .inline)
+            .navigationBarTitle("PhotoGalellry",displayMode: .inline)
+            .navigationBarItems(trailing: Button(action: {
+                self.photoViewModel.URLsseion()
+            }, label: {
+                Text("Session")
+            }))
         }
     }
 }
