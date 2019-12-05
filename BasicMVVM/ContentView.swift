1affd4ef7d7b37f733a869e3eaaa5dedf22e0bf2
yamolean <moririnmanro9608@gmail.com>
2019-12-06 03:05:30 +0900

    add model

--- BasicMVVM/ContentView.swift
+++ BasicMVVM/ContentView.swift
@@ -6,7 +6,25 @@
 //  Copyright © 2019 yamolean. All rights reserved.
 //
 
+//https://jsonplaceholder.typicode.com/photos
+//[
+//{
+//  "albumId": 1,
+//  "id": 1,
+//  "title": "accusamus beatae ad facilis cum similique qui sunt",
+//  "url": "https://via.placeholder.com/600/92c952",
+//  "thumbnailUrl": "https://via.placeholder.com/150/92c952"
+//},...]
+
 import SwiftUI
+
+let url = "https://jsonplaceholder.typicode.com/photos"
+
+struct Course: Identifiable, Decodable {
+    let id = UUID()
+    let title: String
+    let thumbnailUrl: String
+}
 
 struct ContentView: View {
     var body: some View {
